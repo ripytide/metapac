93864c4b33d9999bf796db53322c783421041837
innocentzero <md-isfarul-haque@proton.me>
2024-11-02 15:54:07 +0530

    dont use sudo with flatpaks
    Signed-off-by: innocentzero <md-isfarul-haque@proton.me>

--- src/backends/flatpak.rs
+++ src/backends/flatpak.rs
@@ -148,7 +148,7 @@ impl Backend for Flatpak {
                 .into_iter()
                 .chain(Some("--assumeyes").filter(|_| no_confirm))
                 .chain(packages.keys().map(String::as_str)),
-                Perms::Sudo,
+                Perms::Same,
             )?;
         }
 
