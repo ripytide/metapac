fb9d2f4f515b5818bc70655a85bb413edd8d36c9
ripytide <james.forsterer@gmail.com>
2024-11-10 22:42:19 +0000

    fix: fixed command finding not working on windows

--- src/cmd.rs
+++ src/cmd.rs
@@ -4,15 +4,7 @@ use color_eyre::{eyre::eyre, Result};
 use itertools::Itertools;
 
 pub fn command_found(command: &str) -> bool {
-    if let Ok(path) = std::env::var("PATH") {
-        for p in path.split(':') {
-            let p_str = format!("{}/{}", p, command);
-            if std::fs::metadata(p_str).is_ok() {
-                return true;
-            }
-        }
-    }
-    false
+    which::which(command).is_ok()
 }
 
 #[derive(Debug, Clone, Copy)]
